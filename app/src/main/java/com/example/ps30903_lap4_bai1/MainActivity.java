e0ecaf0624f76dd1c02ff468a0b520092099218d
224pt304 <hoangphuc2004lk@gmail.com>
2023-07-22 13:13:49 +0700

    cau lenh dau tien

---
+++ app/src/main/java/com/example/ps30903_lap4_bai1/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.ps30903_lap4_bai1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnFragment1, btnFragment2;
+    private FrameLayout frameLayout;
+
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnFragment1 = findViewById(R.id.btnFragment1);
+        btnFragment2 = findViewById(R.id.btnFragment2);
+        frameLayout = findViewById(R.id.frameLayout);
+
+        fragmentManager = getSupportFragmentManager();
+        replaceFragment(new Fragment1());
+
+
+        btnFragment1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new Fragment1());
+            }
+        });
+
+        btnFragment2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new Fragment2());
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+}
